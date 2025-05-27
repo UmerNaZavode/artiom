325801167b2e526ca6e89ec731a60bbdbe6cba9a
Artiom <malkocmax@gmail.com>
2025-05-27 14:03:38 +0300

    full desktop + js start

---
+++ script.js
@@ -0,0 +1,36 @@
+// Табы
+
+const tabsBtns = document.querySelectorAll(".tabs__nav button")
+const tabsItems = document.querySelectorAll(".tabs__item")
+
+// Скрывает табы и отбирает класс "active" у кнопки
+function hideTabs() {
+    tabsItems.forEach(item => item.classList.add("hide"));
+    tabsBtns.forEach(item => item.classList.remove("active"));
+}
+
+// Показывает переданный номер таба и делаеи его кнопку активной
+function showTab(index) {
+    tabsItems[index].classList.remove("hide")
+    tabsBtns[index].classList.add("active")
+}
+
+hideTabs();
+showTab(0)
+
+tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+}));
+
+//Якори
+
+const anchors = document.querySelectorAll(".header__nav a")
+
+anchors.forEach(anc =>{
+    anc.addEventListener("click", function(event) {
+       event.preventDefault();
+
+
+    });
+});
